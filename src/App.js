cae86afde3e9f2cd1c8b8b4a31861aa4d18a1897
Diego Lins <diegotec.lins@gmail.com>
2022-10-04 21:50:40 -0300

    teste pipeline 2 Diego Lins

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello Diego Lins!
+          Vamos começar?
         </p>
         <a
           className="App-link"
